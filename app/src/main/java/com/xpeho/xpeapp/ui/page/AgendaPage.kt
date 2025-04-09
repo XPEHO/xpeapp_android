e8a5f835a4a7a02256017072a3196acbd730a00a
Théo Lebègue <theo.lebegue@icloud.com>
2025-04-09 16:44:39 +0200

    fix(UiState): change UI State problem (#11)

--- app/src/main/java/com/xpeho/xpeapp/ui/page/AgendaPage.kt
+++ app/src/main/java/com/xpeho/xpeapp/ui/page/AgendaPage.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.xpeho.xpeapp.R
+import com.xpeho.xpeapp.ui.components.AppLoader
 import com.xpeho.xpeapp.ui.components.CustomDialog
 import com.xpeho.xpeapp.ui.components.agenda.AgendaBirthdayItem
 import com.xpeho.xpeapp.ui.components.agenda.AgendaCardList
@@ -17,37 +18,30 @@ import com.xpeho.xpeapp.ui.components.agenda.AgendaEventItem
 import com.xpeho.xpeapp.ui.components.layout.Title
 import com.xpeho.xpeapp.ui.sendAnalyticsEvent
 import com.xpeho.xpeapp.ui.uiState.AgendaUiState
-import com.xpeho.xpeapp.ui.uiState.QvstUiState
 import com.xpeho.xpeapp.ui.viewModel.agenda.AgendaViewModel
-import com.xpeho.xpeapp.ui.viewModel.viewModelFactory
 
 @Composable
-fun AgendaPage() {
-    val agendaViewModel = viewModel<AgendaViewModel>(
-        factory = viewModelFactory {
-            AgendaViewModel()
-        }
-    )
+fun AgendaPage(agendaViewModel : AgendaViewModel = viewModel()) {
+    val agendaState = agendaViewModel.state
 
     sendAnalyticsEvent("agenda_page")
-
-    LaunchedEffect(Unit) {
-        agendaViewModel.updateState()
-    }
 
     LazyColumn(
         modifier = Modifier
             .padding(horizontal = 24.dp, vertical = 10.dp)
-            .fillMaxSize(),
+            .fillMaxSize()
     ) {
-        when (agendaViewModel.state) {
+        item {
+            Title(label = "Agenda")
+        }
+
+        when (agendaState) {
             is AgendaUiState.SUCCESS -> {
+                val state = agendaState
+                val events = state.agendaEvent.map { AgendaEventItem(it) }
+                val birthdays = state.agendaBirthday.map { AgendaBirthdayItem(it) }
+                val items = (events + birthdays).sortedBy { it.date }
                 item {
-                    Title(label = "Agenda")
-                    val state = agendaViewModel.state as AgendaUiState.SUCCESS
-                    val events = state.agendaEvent.map { AgendaEventItem(it) }
-                    val birthdays = state.agendaBirthday.map { AgendaBirthdayItem(it) }
-                    val items = (events + birthdays).sortedBy { it.date }
                     AgendaCardList(
                         items = items,
                         eventsTypes = state.agendaEventType,
@@ -63,6 +57,15 @@ fun AgendaPage() {
                     ) {
                         agendaViewModel.resetState()
                     }
+                }
+            }
+
+            is AgendaUiState.LOADING -> {
+                item {
+                    LaunchedEffect(Unit) {
+                        agendaViewModel.updateState()
+                    }
+                    AppLoader()
                 }
             }
         }
