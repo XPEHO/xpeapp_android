d135196c6b5aa54f118aa3a2d1f9373127e3e131
Théo Lebègue <theo.lebegue@icloud.com>
2025-10-28 16:03:55 +0100

    hotfix(QVSTCannotBeApproved): change the padding on Next and previous… (#41)

--- app/src/main/java/com/xpeho/xpeapp/ui/page/qvst/QvstCampaignDetail.kt
+++ app/src/main/java/com/xpeho/xpeapp/ui/page/qvst/QvstCampaignDetail.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
@@ -166,6 +167,7 @@ fun QvstCampaignDetailPage(
                             modifier = Modifier
                                 .padding(bottom = 10.dp, top = 5.dp)
                                 .fillMaxWidth()
+                                .safeDrawingPadding()
                         ) {
                             // Previous button
                             Icon(
