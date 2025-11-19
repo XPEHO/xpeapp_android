f7cc6170934f144f48d134b4783f894ca44a0d7e
Théo Lebègue <theo.lebegue@icloud.com>
2025-11-19 16:24:59 +0100

    chore(crashlyticsUtils): rectify crashlytics utils

--- app/src/main/java/com/xpeho/xpeapp/utils/CrashlyticsUtils.kt
+++ app/src/main/java/com/xpeho/xpeapp/utils/CrashlyticsUtils.kt
@@ -1,6 +1,5 @@
 package com.xpeho.xpeapp.utils
 
-import android.util.Log
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 object CrashlyticsUtils {
@@ -14,80 +13,52 @@ object CrashlyticsUtils {
      * Enregistre un événement personnalisé dans Crashlytics.
      */
     fun logEvent(message: String) {
-        try {
-            crashlytics.log(message)
-        } catch (e: Throwable) {
-            Log.e(TAG, "Failed to log event to Crashlytics", e)
-        }
+        crashlytics.log(message)
     }
 
     /**
      * Enregistre une exception non fatale dans Crashlytics.
      */
     fun recordException(exception: Throwable) {
-        try {
-            crashlytics.recordException(exception)
-        } catch (e: Throwable) {
-            Log.e(TAG, "Failed to record exception to Crashlytics", e)
-        }
+        crashlytics.recordException(exception)
     }
 
     /**
      * Définit un identifiant utilisateur pour les crash reports.
      */
     fun setUserId(userId: String) {
-        try {
-            crashlytics.setUserId(userId)
-        } catch (e: Throwable) {
-            Log.e(TAG, "Failed to set user ID", e)
-        }
+        crashlytics.setUserId(userId)
     }
 
     /**
      * Ajoute une clé-valeur personnalisée aux crash reports.
      */
     fun setCustomKey(key: String, value: String) {
-        try {
-            crashlytics.setCustomKey(key, value)
-        } catch (e: Throwable) {
-            Log.e(TAG, "Failed to set custom key ($key)", e)
-        }
+        crashlytics.setCustomKey(key, value)
     }
 
     /**
      * Définit le screen/écran actuel pour contextualiser les erreurs.
      */
     fun setCurrentScreen(screenName: String) {
-        try {
-            crashlytics.setCustomKey("screen", screenName)
-        } catch (e: Throwable) {
-            Log.e(TAG, "Failed to set current screen", e)
-        }
+        crashlytics.setCustomKey("screen", screenName)
     }
 
     /**
      * Définit la feature/fonctionnalité actuelle.
      */
     fun setCurrentFeature(featureName: String) {
-        try {
-            crashlytics.setCustomKey("feature", featureName)
-        } catch (e: Throwable) {
-            Log.e(TAG, "Failed to set current feature", e)
-        }
+        crashlytics.setCustomKey("feature", featureName)
     }
 
     /**
      * Définit le contexte utilisateur (connecté/déconnecté).
      */
     fun setUserContext(isLoggedIn: Boolean, userRole: String = "") {
-        try {
-            crashlytics.setCustomKey("user_logged_in", isLoggedIn.toString())
+        crashlytics.setCustomKey("user_logged_in", isLoggedIn.toString())
 
-            if (userRole.isNotEmpty()) {
-                crashlytics.setCustomKey("user_role", userRole)
-            }
-        } catch (e: Throwable) {
-            Log.e(TAG, "Failed to set user context", e)
+        if (userRole.isNotEmpty()) {
+            crashlytics.setCustomKey("user_role", userRole)
         }
     }
 }
